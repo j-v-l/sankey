a704793579c2cc324f3f4f0c22c762dd00966712
Jim Van Loon <jim.vanloon@tntp.org>
2018-02-22 15:22:30 -0500

    Edit ribbon function
    Modify the ribbon function to create sinusoidal curves instead of straight lines

--- ribbon.R
+++ ribbon.R
@@ -8,10 +8,10 @@ ribbon <- function(x0, x1, y0, y1, h0, h1, stepsize = 0.05) {
   
   x <- seq(x0, x1, by = stepsize)
   nsteps <- length(x)
-  xx <- seq(-pi/2, pi/2, length.out = )
   
-  ymin <- seq(y0, y1, length.out = length(x)) # this will change to a curve
-  ymax <- seq(y0 + h0, y1 + h1, length.out = length(x)) # this will change to a curve
+  xx <- seq(-pi/2, pi/2, length.out = nsteps)
+  ymin <- y0 + (y1 - y0) * (sin(xx) + 1) / 2
+  ymax <- ymin + seq(h0, h1, length.out = nsteps)
   
   data.frame(x, ymin, ymax)
 }
@@ -20,3 +20,4 @@ ribbon <- function(x0, x1, y0, y1, h0, h1, stepsize = 0.05) {
 temp <- ribbon(1, 3, 2, 1, 2, 1.5)
 
 ggplot(temp, aes(x = x)) + geom_ribbon(aes(ymin = ymin, ymax = ymax)) + xlim(0, 4) + ylim(0, 5)
+
