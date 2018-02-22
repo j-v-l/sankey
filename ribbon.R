2003568549b1bb62d61e1f24c5571db9d0cac7ee
Jim Van Loon <jim.vanloon@tntp.org>
2018-02-22 15:36:12 -0500

    Testing different ribbons

--- ribbon.R
+++ ribbon.R
@@ -2,7 +2,7 @@ library(pacman)
 p_load(dplyr, ggplot2, reshape2)
 
 
-ribbon <- function(x0, x1, y0, y1, h0, h1, stepsize = 0.05) {
+ribbon <- function(x0, y0, h0, x1, y1, h1, stepsize = 0.05) {
   # Create a df to pass to geom_ribbon that will draw a ribbon from (x0, y0) to (x1, y1), 
   # with height (above the base points) going from h0 to h1
   
@@ -19,5 +19,9 @@ ribbon <- function(x0, x1, y0, y1, h0, h1, stepsize = 0.05) {
 
 temp <- ribbon(1, 3, 2, 1, 2, 1.5)
 
-ggplot(temp, aes(x = x)) + geom_ribbon(aes(ymin = ymin, ymax = ymax)) + xlim(0, 4) + ylim(0, 5)
+ggplot(data.frame(), aes(x = x, ymin = ymin, ymax = ymax)) + 
+  geom_ribbon(data = ribbon(1, 2, 1.5, 3, 1, 1.5), fill = NA, color = "black") +
+  geom_ribbon(data = ribbon(3, 1, 1, 5, 0.25, 1), fill = NA, color = "black") +
+  geom_ribbon(data = ribbon(3, 2, 0.5, 5, 1.5, 0.5), fill = NA, color = "black") +
+  xlim(0, 6) + ylim(0, 5)
 
